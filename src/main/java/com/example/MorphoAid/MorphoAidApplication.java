2bf26b75a2b66dd0d4605c0db8e2b42a64694944
JiafeiAKA <seksan_jo@cmu.ac.th>
2025-06-18 15:11:48 +0700

    Init backend

---
+++ src/main/java/com/example/MorphoAid/MorphoAidApplication.java
@@ -0,0 +1,13 @@
+package com.example.MorphoAid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorphoAidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorphoAidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MorphoAid/MorphoAidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MorphoAid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorphoAidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
